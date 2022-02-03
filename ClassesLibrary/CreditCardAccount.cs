090adcbf7694e8ac379e9520e3f4e7c37176bc10
Michael Redifer <mikeredifer@yahoo.com>
2022-02-02 19:02:10 -0600

    created creditcardaccount class

---
+++ ClassesLibrary/CreditCardAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class CreditCardAccount
+    {
+        private int _accountNumber;
+
+        private decimal _balance;
+
+        private bool _isPastDue;
+
+        private decimal _annualInterestRate;
+
+
+        public int AccountNumbers { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public bool IsPastDue { get; set; }
+
+        public decimal AnnualInterestRate { get; set; }
+
+
+        public CreditCardAccount(int accountNumbers, )
+
+    }//end class
+}//end namespace
